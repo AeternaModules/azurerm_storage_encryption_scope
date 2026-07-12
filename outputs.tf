362cec14f6ad354bd0076cb72df2fc454358c4dd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:32:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "storage_encryption_scopes_id" {
+  description = "Map of id values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
+  value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.id }
+}
 output "storage_encryption_scopes_infrastructure_encryption_required" {
   description = "Map of infrastructure_encryption_required values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
   value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.infrastructure_encryption_required }
