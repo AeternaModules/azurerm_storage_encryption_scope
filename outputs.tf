16d9717f2d368e9d3df0e4423e04920a24793188
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:48 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "storage_encryption_scopes" {
-  description = "All storage_encryption_scope resources"
-  value       = azurerm_storage_encryption_scope.storage_encryption_scopes
-}
 output "storage_encryption_scopes_infrastructure_encryption_required" {
-  description = "List of infrastructure_encryption_required values across all storage_encryption_scopes"
-  value       = [for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : v.infrastructure_encryption_required]
+  description = "Map of infrastructure_encryption_required values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
+  value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.infrastructure_encryption_required }
 }
 output "storage_encryption_scopes_key_vault_key_id" {
-  description = "List of key_vault_key_id values across all storage_encryption_scopes"
-  value       = [for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : v.key_vault_key_id]
+  description = "Map of key_vault_key_id values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
+  value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.key_vault_key_id }
 }
 output "storage_encryption_scopes_name" {
-  description = "List of name values across all storage_encryption_scopes"
-  value       = [for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : v.name]
+  description = "Map of name values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
+  value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.name }
 }
 output "storage_encryption_scopes_source" {
-  description = "List of source values across all storage_encryption_scopes"
-  value       = [for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : v.source]
+  description = "Map of source values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
+  value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.source }
 }
 output "storage_encryption_scopes_storage_account_id" {
-  description = "List of storage_account_id values across all storage_encryption_scopes"
-  value       = [for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : v.storage_account_id]
+  description = "Map of storage_account_id values across all storage_encryption_scopes, keyed the same as var.storage_encryption_scopes"
+  value       = { for k, v in azurerm_storage_encryption_scope.storage_encryption_scopes : k => v.storage_account_id }
 }
 
